5b16c3bdc5512915e90d1802692bb5c5ea4745c0
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2019-01-11 20:46:45 +0100

    Added Thread Safe Singleton example

---
+++ ThreadSafeSingleton/ConsoleLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThreadSafeSingleton
+{
+    class ConsoleLogger
+    {
+        private static object _lock = new object();
+        private static volatile  ConsoleLogger _instance;
+        private volatile int _counter = 0;
+
+        private ConsoleLogger() {}
+
+        public static ConsoleLogger Instance()
+        {
+            if (_instance != null) return _instance;
+            lock (_lock)
+            {
+                if (_instance != null) return _instance;
+                _instance = new ConsoleLogger();
+            }
+
+            return _instance;
+        }
+
+        public void Log()
+        {
+            lock (_lock)
+            {
+                Console.WriteLine($"Value of counter:  {_counter++} from instance {GetHashCode()}");
+            }
+        }
+    }
+}
---
+++ ThreadSafeSingleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadSafeSingleton
+{
+    internal class Program
+    {
+        public static void Work()
+        {
+            Thread.Sleep(10);
+            ConsoleLogger.Instance().Log();
+        }
+
+        private static void Main(string[] args)
+        {
+            var start = new ThreadStart(Work);
+            for (var i = 0; i < 10; i++) new Thread(start).Start();
+            Console.ReadKey();
+        }
+    }
+}
